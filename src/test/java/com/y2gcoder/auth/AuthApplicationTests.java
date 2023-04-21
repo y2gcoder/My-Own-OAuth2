4e0824dcaf3b55f05ec9833795c5dc4c66364026
y2gcoder <y2gcoder@gmail.com>
2023-04-21 13:30:57 +0900

    Initial commit

---
+++ src/test/java/com/y2gcoder/auth/AuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.y2gcoder.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
